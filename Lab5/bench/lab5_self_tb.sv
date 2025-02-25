850fd45b1e9af33f43899faaa2c1e29cb293b44e
Muhammad-Abdullahh <muhammadabdullah19450@gmail.com>
2025-02-25 12:52:19 -0800

    Commit message

--- Lab5/bench/lab5_self_tb.sv
+++ Lab5/bench/lab5_self_tb.sv
@@ -52,23 +52,23 @@ module lab5_self_tb;
         logic [6:0] expected_led;
         begin
             case ({a, b, c, d})
-                 4'b0000: expected_led = 7'b0000001;
-                 4'b0001: expected_led = 7'b0000110;
-                 4'b0010: expected_led = 7'b1011011;
-                 4'b0011: expected_led = 7'b1101111;
-                 4'b0100: expected_led = 7'b1100110;
-                 4'b0101: expected_led = 7'b1101101;
-                 4'b0110: expected_led = 7'b1111101;
-                 4'b0111: expected_led = 7'b0000111;
-                 4'b1000: expected_led = 7'b1111111;
-                 4'b1001: expected_led = 7'b1100111;
-                 4'b1010: expected_led = 7'b1110111;
-                 4'b1011: expected_led = 7'b1111100;
-                 4'b1100: expected_led = 7'b0111001;
-                 4'b1101: expected_led = 7'b1011110;
-                 4'b1110: expected_led = 7'b0111111;
-                 4'b1111: expected_led = 7'b0110111;
-                  default: expected_led = 7'b0000000;
+            4'b0000: expected_led = 7'b0000001;
+            4'b0001: expected_led = 7'b0000110;
+            4'b0010: expected_led = 7'b1011011;
+            4'b0011: expected_led = 7'b1001111;
+            4'b0100: expected_led = 7'b1100110;
+            4'b0101: expected_led = 7'b1101101;
+            4'b0110: expected_led = 7'b1111101;
+            4'b0111: expected_led = 7'b0000111;
+            4'b1000: expected_led = 7'b1111111;
+            4'b1001: expected_led = 7'b1101111;
+            4'b1010: expected_led = 7'b1110111;
+            4'b1011: expected_led = 7'b1111100;
+            4'b1100: expected_led = 7'b0111001;
+            4'b1101: expected_led = 7'b1011110;
+            4'b1110: expected_led = 7'b1111001;
+            4'b1111: expected_led = 7'b1110001;
+                default: expected_led = 7'b0000000;
             endcase
 
             if ({A, B, C, D, E, F, G} !== expected_led) begin
